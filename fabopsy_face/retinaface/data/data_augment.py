2850c7de72e53961d476bf27cf9bd1c7fae0756c
levalup <likier@sina.cn>
2026-05-27 11:22:26 +0800

    update package import

--- fabopsy_face/retinaface/data/data_augment.py
+++ fabopsy_face/retinaface/data/data_augment.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import random
-from utils.box_utils import matrix_iof
+from ..utils.box_utils import matrix_iof
 
 
 def _crop(image, boxes, labels, landm, img_dim):
@@ -60,7 +60,7 @@ def _crop(image, boxes, labels, landm, img_dim):
         landms_t = landms_t.reshape([-1, 10])
 
 
-	# make sure that the cropped image contains at least one face > 16 pixel at training image scale
+        # make sure that the cropped image contains at least one face > 16 pixel at training image scale
         b_w_t = (boxes_t[:, 2] - boxes_t[:, 0] + 1) / w * img_dim
         b_h_t = (boxes_t[:, 3] - boxes_t[:, 1] + 1) / h * img_dim
         mask_b = np.minimum(b_w_t, b_h_t) > 0.0
